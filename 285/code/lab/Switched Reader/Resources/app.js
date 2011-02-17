5b2749d2786c05d4fa767eda554279af548e8159
Phillip Williams <p@bafmin.com>
2011-02-17 18:31:28 -0500

    updated slides

--- 285/code/lab/Switched Reader/Resources/app.js
+++ 285/code/lab/Switched Reader/Resources/app.js
@@ -26,7 +26,6 @@ var aboutWindow = Titanium.UI.createWindow({
     backgroundColor:'#fff'
 });
 
-
 var html_data = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
 	html_data += '<html xmlns="http://www.w3.org/1999/xhtml">';
 	html_data += '<head>';
