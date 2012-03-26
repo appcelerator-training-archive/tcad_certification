d0acdaa9e32a6102baa7889dcafeeed2773513b5
Tim Poulsen <tim@skypanther.com>
2012-03-26 10:43:52 -0400

    HTML5 slide templates uploaded

---
+++ 000/i18n/localizations.js
@@ -0,0 +1,30 @@
+/*
+	Localization strings file. To translate, copy the entire "en-US" branch, paste in as new member of the object, 
+	set its name to be the two-letter language code, and translate each string.
+	As shown, strings can include simple HTML.
+*/
+String.toLocaleString({
+	"en-US": {
+		"%l_filetitle": "This is the lesson title",
+		"%l_lessontitle": "Lesson Title",
+		"%l_subtitle": "Lesson <b>subtitle</b> goes here, remove HTML tag if none",
+		"%l_slidenote_titleslide": "add presenter notes here",
+		"%l_agenda_title": "AGENDA",
+		"%l_agenda1": "What is Titanium?",
+		"%l_agenda2": "How does Titanium work?",
+		"%l_agenda3": "What can Titanium do?",
+		"%l_agenda4": "What's it like developing with Titanium?",
+		"%l_agenda5": "How can I learn more?",
+		"%l_slidenote_agenda": "You can include <strong>HTML tags</strong> in your <em>presenter</em> notes.",
+		"%l_sample": "Sample Slide",
+		"%l_sample1": "List item 1",
+		"%l_sample1": "List item 2",
+		"%l_sample1": "List item 3",
+		"%l_slidenote_sample": "add presenter notes here",
+		"%l_qa": "Q&amp;A",
+		"%l_lab": "Lab Objectives",
+		"%l_labobjective1": "Lab objective 1",
+		"%l_labobjective2": "Lab objective 2",
+		"%l_labobjective3": "Lab objective 3"
+	}
+});
---
+++ i18n/localizations.js
@@ -0,0 +1,22 @@
+/*
+	Localization strings file. To translate, copy the entire "en-US" branch, paste in as new member of the object, 
+	set its name to be the two-letter language code, and translate each string.
+	As shown, strings can include simple HTML.
+*/
+String.toLocaleString({
+	"en-US": {
+		"%l_welcome": "Welcome To",
+		"%l_coursename": "This is the course name",
+		"%l_agenda_title": "AGENDA",
+		"%l_lesson1": "Lesson 1 Title",
+		"%l_lesson2": "Lesson 2 Title",
+		"%l_lesson3": "Lesson 3 Title",
+		"%l_lesson4": "Lesson 4 Title",
+		"%l_lesson5": "Lesson 5 Title",
+		"%l_lesson6": "Lesson 6 Title",
+		"%l_lesson7": "Lesson 7 Title",
+		"%l_lesson8": "Lesson 8 Title",
+		"%l_lesson9": "Lesson 9 Title",
+		"%l_lesson10": "Lesson 10 Title"
+	}
+});
