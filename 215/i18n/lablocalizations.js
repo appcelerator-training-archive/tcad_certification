1b79fa063303a9a8373b86a97d675976ad68c9a8
Tim Poulsen <tim@skypanther.com>
2012-04-09 17:19:26 -0400

    Lesson 215 updates and lab

---
+++ 215/i18n/lablocalizations.js
@@ -0,0 +1,20 @@
+/*
+	Localization strings file. To translate, copy the entire "en-US" branch, paste in as new member of the object, 
+	set its name to be the two-letter language code, and translate each string.
+	As shown, strings can include simple HTML.
+*/
+String.toLocaleString({
+	"en-US": {
+		"%l_labname": "Lab - Cross-Platform Mobile Development",
+		"%l_mission": "<strong>Mission: </strong>In this lab assignment, you will begin to create a cross-platform mobile application for Android and iPhone devices called TiBountyHunter.  TiBountyHunter is derived from an example application developed in the iOS development primer Head First iPhone Development.  The TiBountyHunter application enables bounty hunters to track a list of fugitives on their mobile device.  In this lab, you will be asked to create and begin implementing this example application.",
+
+		"%l_summary": "Summary",
+		"%l_summarypara": "This lab was a brief and simple introduction to the Xcode development tools.",
+
+		"%l_resources": "Resources",
+		"%l_resources1": "Localizing Strings: <a href='http://wiki.appcelerator.org/display/guides/Internationalizing+your+Application'>wiki.appcelerator.org/display/guides/Internationalizing+your+Application</a>",
+		"%l_resources2": "Titanium Window Reference: <a href='http://developer.appcelerator.com/apidoc/mobile/latest/Titanium.UI.Window-object'>developer.appcelerator.com/apidoc/mobile/latest/Titanium.UI.Window-object</a>",
+		"%l_resources3": "CommonJS modules in Titanium: <a href='http://docs.appcelerator.com/titanium/2.0/index.html#!/guide/CommonJS_Modules_in_Titanium'>docs.appcelerator.com/titanium/2.0/index.html#!/guide/CommonJS_Modules_in_Titanium</a>",
+
+	}
+});
