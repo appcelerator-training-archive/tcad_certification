137b88556a7418755b3234c4f9048eb5c799e7f0
Tim Poulsen <tim@skypanther.com>
2012-04-11 14:16:05 -0400

    Lesson 285 done

---
+++ 285/i18n/lablocalizations.js
@@ -0,0 +1,47 @@
+/*
+	Localization strings file. To translate, copy the entire "en-US" branch, paste in as new member of the object, 
+	set its name to be the two-letter language code, and translate each string.
+	As shown, strings can include simple HTML.
+*/
+String.toLocaleString({
+	"en-US": {
+		"%l_labname": "Lab: Integrating Web Content",
+		
+		"%l_mission": "<strong>Mission: </strong>In this lab, you will integrate web content into a partially-completed RSS reader. Individual articles will be displayed in a web view within a modal window. A local HTML file will be shown in an 'About' tab.",
+		"%l_specification": "<strong>Specification: </strong>You will download a partially-completed file and add the missing functionality. The finished app will look like this:<br/><img src='labimages/webview1.png' height='300' align='left'><img src='labimages/webview2.png' height='300' align='left'><img src='labimages/webview3.png' height='300' align='left'>",
+
+		"%l_step1": "1. Download the starting file set from <a href='http://assets.appcelerator.com.s3.amazonaws.com/app_u/code/TiBH285.zip'>http://assets.appcelerator.com.s3.amazonaws.com/app_u/code/TiBH285.zip</a> and import the resulting files as a new Titanium Mobile project.",
+		"%l_stepexplanation1": "",
+
+		"%l_step2": "2. Examine the contents of the app.js, ui/TabGroup.js, and lib/network.js files. These files have been completed for you.",
+		"%l_stepexplanation2": "We've written most of the UI features and network functions so that you can concentrate on adding the web view functionality. Of course, at this point, the app doesn't do anything.",
+
+		"%l_step3": "3. Open the <strong>ui/RSSWindow.js</strong> file. In the table event listener, follow the code comments to require in the appropriate module, instantiate the window, and open it.",
+		"%l_stepexplanation3": "When users tap on a row, this window will open and display the contents of the blog article.",
+
+		"%l_step4": "4. Open <strong>ui/ArticleWindow.js</strong> and following the comments in that file, write the code to define the window that will contain the web view for showing the blog articles.",
+		"%l_stepexplanation4": "",
+
+		"%l_step5": "5. Open <strong>ui/AboutWindow.js</strong> and following the comments in that file, write the code to define the window that will display the index.html file on the 'About' tab.",
+		"%l_stepexplanation5": "",
+
+		"%l_step6": "6. 6. Save your changes in all files and build your project for the simulator/emulator.",
+		"%l_stepexplanation6": "",
+
+		"%l_step7": "7. Test your work by viewing an article or two and by displaying the About tab.",
+		"%l_stepexlpanation7": "",
+
+		"%l_step8": "8. Close the simulator/emulator and all open files in Studio.",
+		"%l_stepexplanation8": "",
+
+
+		"%l_summary": "Summary",
+		"%l_summarypara": "In this lab, you implemented web views in two different ways. In one, the contents of a blog post are displayed in a web view. In the other, the contents of a local HTML file are displayed within a web view to illustrate how you can display formatted text in that manner.",
+
+		"%l_resources": "Resources",
+		"%l_resources1": "API docs: WebView: <a href='http://docs.appcelerator.com/titanium/2.0/index.html#!/api/Titanium.UI.WebView'>http://docs.appcelerator.com/titanium/2.0/index.html#!/api/Titanium.UI.WebView</a>",
+		"%l_resources2": "Guides: Integrating Web Content: <a href='http://docs.appcelerator.com/titanium/2.0/index.html#!/guide/Integrating_Web_Content'>http://docs.appcelerator.com/titanium/2.0/index.html#!/guide/Integrating_Web_Content</a>",
+		"%l_resources3": "Finished code: <a href='https://github.com/appcelerator-training/tcad-code/tree/master/Finished_code/TiBH285'>GitHub</a>",
+
+	}
+});
--- 285/i18n/localizations.js
+++ 285/i18n/localizations.js
@@ -7,13 +7,16 @@ String.toLocaleString({
 	"en-US": {
 		"%l_filetitle": "Integrating Web Content",
 		"%l_lessontitle": "Integrating Web Content",
-		"%l_slidenote_titleslide": "<b>Module time: 60 mins</b>(Teaching: 15 mins, lab: 45 mins)",
+		"%l_subtitle": "Building Native Mobile Apps With Appcelerator Titanium",
+		"%l_slidenote_titleslide": "<b>Module time: 60 mins</b><br/>(Teaching: 15 mins, lab: 45 mins)",
 		"%l_agenda_title": "Agenda",
+		
 		"%l_agenda1": "Integrating the WebView component",
 		"%l_agenda2": "WebView gotchas",
 		"%l_agenda3": "Novel uses of the WebView",
 		"%l_agenda4": "Lab Exercise",
 		"%l_slidenote_agenda": "",
+
 		"%l_integrating_web": "Integrating Web Content",
 		"%l_html_embed": "HTML/CSS are embedded in much data on the web",
 		"%l_html_often": "HTML/CSS are often the only/best way to accomplish certain tasks:",
@@ -22,35 +25,42 @@ String.toLocaleString({
 		"%l_integrating": "Integrating existing web-based tools",
 		"%l_walking": "Walking HTML documents",
 		"%l_slidenote_integrating_web": "Many business-use cases for using HTML/CSS in your app<br>Might be easiest way to go<br>Canvas for animations<br>Existing libraries (graphing, jquery)<br>DOM manipulations and access",
+
 		"%l_remote_web": "Remote Web Content",
 		"%l_uses_might": "Uses might include pulling in blog content from an RSS feed (hint) or re-purposing (semi) static content from an existing site",
 		"%l_slidenote_remote_web": "Static / existing web pages<br>Dynamic data from a CMS or web service<br>Embed a web view anywhere in your app (views, windows)",
+
 		"%l_local_web": "Local Web Content",
 		"%l_webkit_can": "Webkit can sometimes do something we can't, or we need to re-use existing code (ours or someone else's) - in those cases, we can ship HTML/CSS/JS with our app",
 		"%l_slidenote_local_web": "Integrated webkit has lots of power and capabilities<br>Easy way to embed custom UI, pre-existing content, formatted text<br>Point URL to local web content file<br>Loaded relative to Resources directory",
+
 		"%l_local_web_inline": "Local Web Content - Inline",
 		"%l_sometimes": "Sometimes HTML markup is the best way to display small chunks of text or links (Android handles both natively)",
 		"%l_slidenote_local_web_inline": "Use HTML property for inline content<br>Native Android support for HTML in labels and some other components<br>E.g. linkify() method will turn links and phone numbers into clickable links<br>See the API docs for more information",
+
 		"%l_communicating_wrapper": "Communicating With The Wrapper",
 		"%l_remote_web_access": "Remote web content has no access to Titanium APIs",
 		"%l_limited": "Limited Titanium API access within local web pages:",
 		"%l_logging": "Logging APIs (Ti.API.info and friends)",
 		"%l_app_level": "Application Level Events<br> (Ti.App.fireEvent/addEventListener)",
 		"%l_native_api": "Native APIs must be accessed indirectly via application level events",
-		"%l_slidenote_communicating_wrapper": "Embedded webview is its own execution context<br>Use custom events to communicate with rest of the app<br>For example, geolocation: grab data in native app, fire event passing results to webview<br>Remote content doesn’t have access to Ti APIs<br>But you can inject some JS into a remote page to give limited access",
+		"%l_slidenote_communicating_wrapper": "Embedded webview is its own execution context<br>Use custom events to communicate with rest of the app<br>For example, geolocation: grab data in native app, fire event passing results to webview<br>Remote content doesn't have access to Ti APIs<br>But you can inject some JS into a remote page to give limited access",
+
 		"%l_webview_gotchas": "WebView Gotchas",
 		"%l_among_the": "Among the most expensive components to create",
 		"%l_should_not": "Should not be embedded in other scrollable views",
 		"%l_common_misuse": "Common misuse - inside TableViewRows (bad performance)",
 		"%l_its_possible": "It is possible to implement your whole UI in HTML/CSS, but native UI is what your users expect",
 		"%l_cant_download": "Can't download/run unapproved code from a server",
-		"%l_slidenote_webview_gotchas": "Scrollable views, table views – Touch events can get messed up, bad performance",
+		"%l_slidenote_webview_gotchas": "Scrollable views, table views &mdash; Touch events can get messed up, bad performance",
+
 		"%l_novel_webview": "Novel Uses of the WebView",
 		"%l_display_text": "Display text with embedded links (think Twitter client)",
 		"%l_display_pdf": "Display PDF documents and more (iOS, see next slide)",
 		"%l_retrieve_parse": "Retrieve and parse HTML using the DOM (easier than in native Ti code, where you'd need to do string gymnastics)",
 		"%l_longer_forms": "Longer forms on iOS (next/previous buttons, auto resizing)",
-		"%l_slidenote_novel_webview": "iOS – view PDF (built into Safari) on Android, has to be done through 3rd party app<br>Parsing HTML – use jquery or other libraries to walk the DOM<br>Forms - auto resizing, auto-scrolling, auto “Next” button on iOS to ease data entry.",
+		"%l_slidenote_novel_webview": "iOS &mdash; view PDF (built into Safari) on Android, has to be done through 3rd party app<br>Parsing HTML &mdash; use jquery or other libraries to walk the DOM<br>Forms - auto resizing, auto-scrolling, auto 'Next' button on iOS to ease data entry.",
+
 		"%l_embedded_docs": "Embedded Documents on iOS",
 		"%l_webview_ios": "WebView on iOS can display embedded:",
 		"%l_excel": "Excel",
@@ -63,16 +73,18 @@ String.toLocaleString({
 		"%l_rtf_dir": "RTF Directory",
 		"%l_pdf": "PDF",
 		"%l_slidenote_embedded_docs": "Source: http://developer.apple.com/library/ios/#qa/qa1630/_index.html",
+
 		"%l_qa": "Q&A",
 		"%l_slidenote_qa": "",
+
 		"%l_lab_goals": "Lab Goals",
 		"%l_requirement": "Requirements:",
 		"%l_pull_in": "Pull in an RSS feed and place the results in a TableView",
 		"%l_create_an": "Create an 'about' page using local HTML",
-		"%l_demo_lab": "Demo and Lab URL",
 		"%l_demo_wiki": "Demo and Wiki URL",
-		"%l_slidenote_lab_goals": "Demo – Lab285:</b><ol><li>Show scrolling list of news items</li><li>Click one to show web view</li><li>Click About tab to show local HTML data</li></ol><br>Switched RSS feed might have gone stale, use Dev blog instead:<br>http://developer.appcelerator.com/blog/feed",
+		"%l_slidenote_lab_goals": "Demo &mdash; Lab285:</b><ol><li>Show scrolling list of news items</li><li>Click one to show web view</li><li>Click About tab to show local HTML data</li></ol><br/>The UI, network, and RSS feed parsing is done already.",
+
 		"%l_lab_exercise": "Lab Exercise",
-		"%l_slidenote_lab_exercise": "<b>Code Walkthrough – Lab285</b><br>app.js<ul><li>show URL at top</li><li>xhr.onload ~line 66</li><ul><li>xml = this.responseXML</li><li>talk through walking the XML DOM</li><li>loop through items array to build table rows</li><li>point out custom <b>row.link</b> property</li><li>tableview event listener</li><li>custom property <b>(win.myurl)</b> on window</li></ul></ul><u>showweb.js</u><ul><li>creating the web view (line 2)</li><li>loads url passed in via custom window property</li></ul>Scroll back to top to point out local HTML<br>Could load from about.html instead"
+		"%l_slidenote_lab_exercise": "<b>Code Walk through &mdash; TiBH285</b><br><ul><li>RSSWindow &mdash; table event listener, e.rowData argument, opening modal window, also App-level event listener to load articles once data is retrieved from the network</li><li>ArticleWindow &mdash; 'article' parameter, webview url property</li><li>AboutWindow &mdash; webview url property</li></ul>"
 	}
 });
