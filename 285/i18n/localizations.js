11f253c2dadd22e98ae760f718e7d724840d2fad
kazkonno <devkonno@gmail.com>
2012-04-28 19:21:03 +0900

    First version

--- 285/i18n/localizations.js
+++ 285/i18n/localizations.js
@@ -86,5 +86,89 @@ String.toLocaleString({
 
 		"%l_lab_exercise": "Lab Exercise",
 		"%l_slidenote_lab_exercise": "<b>Code Walk through &mdash; TiBH285</b><br><ul><li>RSSWindow &mdash; table event listener, e.rowData argument, opening modal window, also App-level event listener to load articles once data is retrieved from the network</li><li>ArticleWindow &mdash; 'article' parameter, webview url property</li><li>AboutWindow &mdash; webview url property</li></ul>"
+	},
+	"ja": {
+		"%l_filetitle": "Webコンテンツとの統合",
+		"%l_lessontitle": "Webコンテンツとの統合",
+		"%l_subtitle": "Appcelerator Titaniumによるネイティブアプリ開発",
+		"%l_slidenote_titleslide": "<b>モジュールタイム: 60分</b><br/>(講習: 15分, 実習: 45分)",
+		"%l_agenda_title": "アジェンダ",
+		
+		"%l_agenda1": "WebViewコンポーネントとの統合",
+		"%l_agenda2": "WebViewの落とし穴",
+		"%l_agenda3": "WebViewの面白い使い方",
+		"%l_agenda4": "実習",
+		"%l_slidenote_agenda": "",
+
+		"%l_integrating_web": "Webコンテンツとの統合",
+		"%l_html_embed": "HTML/CSSは、Web上の多くのデータの中で利用されている",
+		"%l_html_often": "HTML/CSSは、以下のタスクを実行するのに唯一もしくは最適な方法である",
+		"%l_rendering": "HTML/CSSの表示",
+		"%l_canvas": "Canvas（アニメーション）",
+		"%l_integrating": "既存Webベースのツールとの統合",
+		"%l_walking": "HTMLドキュメントの解析",
+		"%l_slidenote_integrating_web": "アプリ内でHTMLを利用する多くのビジネスユースの場合<br>アニメーションにはCanvasを使うのが最も簡単な方法。<br>既存のライブラリ（graphing, jquery）<br>DOM操作や取得",
+
+		"%l_remote_web": "リモートWebコンテンツ",
+		"%l_uses_might": "RSSフィードからブログコンテンツを取得したり、既存サイトの静的コンテンツを表示",
+		"%l_slidenote_remote_web": "静的／既存のWebページ<br>CMSやWebサービスからの動的データ<br>アプリ内（views、Windows）のどこでもWeb viewを埋め込むことが可能",
+
+		"%l_local_web": "ローカルWebコンテンツ",
+		"%l_webkit_can": "Webkitは、Titanium Mobileでは出来ないことを実行できたり、既存のコードを再利用する必要があり、HTML/CSS/JSをアプリ内に組み込む事が可能",
+		"%l_slidenote_local_web": "統合されたWebkitには多くの機能が備わっている<br>カスタムUIや既存のコンテンツ、フォーマットされたテキストをアプリ内に組み込むのが簡単<br>ローカルWebコンテンツファイルのURLを設定<br>Resourcesディレクトリの読み込み",
+
+		"%l_local_web_inline": "ローカルWebコンテンツ - インライン",
+		"%l_sometimes": "HTMLマークアップは、ちょっとしたテキストやリンクを表示するのに最適（Androidではネイティブでサポート）",
+		"%l_slidenote_local_web_inline": "インラインコンテンツにはHTMLプロパティーを使用<br>AndroidのラベルなどのコンポーネントにおけるHTMLはネイティブサポート<br>例)  linkify()メソッドは、リンクや電話番号をクリッカブルリンクにする<br>詳細についてAPIドキュメントを参照",
+
+		"%l_communicating_wrapper": "ラッパーとの通信",
+		"%l_remote_web_access": "リモートWebコンテンツからTitanium APIを呼びさせない",
+		"%l_limited": "ローカルWebページ内では限定的にTitanium APIを呼び出せる",
+		"%l_logging": "ロギングAPI (Ti.API.info等)",
+		"%l_app_level": "アプリケーションレベルのイベント<br> (Ti.App.fireEvent/addEventListener)",
+		"%l_native_api": "Titanium APIへは、アプリケーションレベルのイベント経由で間接的にアクセスしなければならない",
+		"%l_slidenote_communicating_wrapper": "組み込んだwebviewには、独自の実行コンテキストがある<br>他のアプリとの通信にはカスタムイベントを使用する<br>例えば、ジオロケーション：ネイティブアプリでデータを取得し、イベントを発動させて結果をwebviewへ渡す<br>リモートコンテンツはTitanium APIを呼び出せない<br>しかし、限定的な呼び出しのため、リモートページにJSを差し込むことが可能",
+
+		"%l_webview_gotchas": "WebViewの落とし穴",
+		"%l_among_the": "最もリソースが消費されるコンポーネント",
+		"%l_should_not": "スクロール可能なviewに組み込むべきではない",
+		"%l_common_misuse": "よくある間違った使い方 -TableViewRows内での利用 (パフォーマンス問題)",
+		"%l_its_possible": "HTML/CSSでアプリ全体のUIを作成することは可能ですが、ユーザはネイティブUIを期待する",
+		"%l_cant_download": "サーバーからの未承認コードをダウンロードし、実行することができない",
+		"%l_slidenote_webview_gotchas": "スクロール可能なview、テーブルview &mdash; タッチイベントが正しく動作しない、パフォーマンス問題",
+
+		"%l_novel_webview": "WebViewの面白い使い方",
+		"%l_display_text": "リンクの付いたテキストの表示（Twitterクライアント）",
+		"%l_display_pdf": "PDFドキュメント等の表示（iOS、次のスライド参照）",
+		"%l_retrieve_parse": "DOMを使用したHTMLの取得とパース（Titanium APIより簡単）",
+		"%l_longer_forms": "iOSのフォーム（前後ボタン、自動リサイズ）",
+		"%l_slidenote_novel_webview": "iOS &mdash; AndroidでのPDF表示（Safariは対応）は、サードパーティーを利用する必要がある<br>HTMLのパース &mdash; DOMを解析にはjquery等のライブラリを使用<br>フォーム - iOSでは、データエントリーを楽にするための、自動リサイズ、自動スクロール、自動「次」ボタンがある",
+
+		"%l_embedded_docs": "iOSのドキュメント表示",
+		"%l_webview_ios": "iOSのWebViewでは以下のフォーマットが表示可能：",
+		"%l_excel": "Excel",
+		"%l_powerpoint": "Excel",
+		"%l_keynote": "Keynote",
+		"%l_word": "Word",
+		"%l_numbers": "Numbers",
+		"%l_rtf": "RTF",
+		"%l_pages": "Pages",
+		"%l_rtf_dir": "RTF Directory",
+		"%l_pdf": "PDF",
+		"%l_slidenote_embedded_docs": "ソース: http://developer.apple.com/library/ios/#qa/qa1630/_index.html",
+
+		"%l_qa": "Q&A",
+		"%l_slidenote_qa": "",
+
+		"%l_lab_goals": "実習内容",
+		"%l_create_rss": "XMLフィードを取得し、Webviewでブログ記事を表示するRSSリーダーアプリを構築",
+		"%l_requirement": "必要条件:",
+		"%l_pull_in": "RSSフィードを取得し、結果をTableViewに表示",
+		"%l_create_an": "ローカルHTMLを利用して'about'ページを作成",
+		"%l_demo_wiki": "デモと実習URL",
+		"%l_slidenote_lab_goals": "デモ &mdash; Lab285:</b><ol><li>新しいアイテムのリストをスクロール表示</li><li>クリックしてWebviewを表示</li><li>AboutタブをクリックしてローカルHTMLデータを表示</li></ol><br/>UI、ネットワーク、そしてRSSフィードの解析が完了",
+
+		"%l_solution": "実習",
+		"%l_slidenote_solution": "<b>コードウォークスルー &mdash; TiBH285</b><br><ul><li>RSSウィンドウ &mdash; テーブルイベントリスナー, e.rowDataに引数, モーダルウィンドウを開く, また、データがネットワークから取得できたらアプリレベルのイベントリスナーで記事を読み込む</li><li>ArticleWindow &mdash; '記事' パラメーター, webview urlプロパティー</li><li>AboutWindow &mdash; webview url プロパティー</li></ul>"
 	}
 });
